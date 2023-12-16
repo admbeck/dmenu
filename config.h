c5a2a30c8958bb16ab614852da7a96d19b5ac686
AdmBeck <admbeck@outlook.com>
2023-12-16 21:32:25 +0500

    changed color

--- config.h
+++ config.h
@@ -13,7 +13,7 @@ static const char *prompt      = NULL;      /* -p  option; prompt to the left of
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
 	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#ce7e00" },
+	[SchemeSel] = { "#000000", "#ce7e00" },
 	[SchemeOut] = { "#000000", "#00ffff" },
 };
 static const unsigned int alphas[SchemeLast][2] = {
